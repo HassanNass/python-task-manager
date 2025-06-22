135afee220659e5f3b592a685c9fa78296ccc3a6
unknown <hassannass613@gmail.com>
2025-06-22 17:49:39 +0300

    Add code to take inputs

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 def insert(queue, task):
     queue.append(task)
 
+queue = []
+
+num_of_tasks = int(input("How many tasks do you want to add?: "))
+
+for i in range(num_of_tasks):
+    print(f"\nTask {i+1}:")
+    title = input("Enter task title: ")
+    duration = int(input("Enter the duration of the task in minutes: "))
+    priority = int(input("Enter priority number (The lower the number, the higher the priority): "))
+    
+    task = {
+        "title": title,
+        "duration": duration,
+        "priority": priority
+    }
+    
+    insert(queue, task)
+    
